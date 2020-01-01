cfcc3efd2d3104ec6c69f6cd0711a68db39db605
Giordanobass <giordanoamaral@gmail.com>
2020-01-01 18:39:34 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
-	
+
 	}
 
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,10 +1,10 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -30,5 +30,5 @@ public class Position {
 	public String toString() {
 		return row + ", " + column;
 	}
-	
+
 }
